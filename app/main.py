8c1d111b7d0ce3129b0dcb7241854834fa9d6030
GungIndi <gungindikusumaputra@gmail.com>
2024-11-20 15:47:23 +0800

    feat: add api endpoints

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.schemas.schema import Journal
+
 import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
+@app.get('/')
 async def index():
     return {"message": "Hello Memothians"}
+
+@app.post('/predict')
+async def feedback(journal: Journal):
+    return 'for predict'
+
+@app.post('/feedback')
+async def feedback(journal: Journal):
+    return 'for feedback'
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
