9f2f33aedb7f8ddebdad87ebc276c3c5c607ecee
GungIndi <gungindikusumaputra@gmail.com>
2024-11-22 13:15:10 +0800

    feat: added predict services to handle emotion prediction

---
+++ app/services/predict.py
@@ -0,0 +1,34 @@
+import numpy as np
+import json
+from app.schemas.schema import JournalSchema, Emotion, EmotionItem
+from app.services.feedback import feedback_service
+
+async def predict_service(journal: JournalSchema, model) -> JournalSchema:
+    threshold = 0.2
+    try:
+        # Format data
+        input_data = np.array([journal.journal]).astype("object")
+        print(f"Input data: {input_data}, type: {type(input_data)}")
+        
+        # Predict
+        predictions = model.predict(input_data)
+        print(f"Prediction result: {predictions}")
+        
+        # Identify classes above the threshold
+        emotion_classes = [emotion.value for emotion in Emotion]
+        emotion_data = [
+            EmotionItem(result=emotion_classes[i], confidence=round(float(conf), 3))
+            for i, conf in enumerate(predictions[0]) if conf > threshold
+        ]
+        
+        # Assign emotion into journalSchema
+        journal.emotion =  emotion_data
+
+        print(f"Formatted prediction response: {journal}")
+        journal = await feedback_service(journal, model)
+        return journal
+    
+    except Exception as e:
+        # Handle and log errors
+        print(f"Error during prediction: {e}")
+        raise ValueError(f"Failed to generate prediction: {e}")
