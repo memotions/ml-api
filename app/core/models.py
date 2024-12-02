e5e46dfd8cdeb22993f8bb848cbf2c28868e1cf8
GungIndi <gungindikusumaputra@gmail.com>
2024-12-02 19:07:44 +0800

    refactor: change how the model is load and how input is being processed

--- app/core/models.py
+++ app/core/models.py
@@ -1,15 +1,20 @@
 import gc
 import tensorflow as tf
+
+
 from app.core.logging_config import setup_logging
 from app.core.response import json_response
 
 logger, _ = setup_logging()
 
 
-async def load_model(MODEL_URL):
+async def load_model(MODEL_URL, TFBertModel):
     try:
         logger.debug(f"Load model from {MODEL_URL}...")
-        model = tf.keras.models.load_model(MODEL_URL)
+        model = tf.keras.models.load_model(
+            "mood_classifier_bert.keras",
+            custom_objects={"TFBertModel": TFBertModel},
+        )
         return model
 
     except Exception as e:
--- app/services/predict.py
+++ app/services/predict.py
@@ -1,3 +1,5 @@
+import os
+from transformers import BertTokenizer
 import numpy as np
 from fastapi import HTTPException
 from datetime import datetime
@@ -7,9 +9,12 @@ from app.schemas.schema import JournalSchema, Emotion, EmotionItem
 from app.services.feedback import feedback_service
 
 logger, _ = setup_logging()
+MAX_LENGTH = 64
 
 
 async def predict_service(journal: JournalSchema, model) -> JournalSchema:
+    tokenizer = BertTokenizer.from_pretrained(os.getenv("BERT_MODEL_NAME"))
+    logger.debug(f"Tokenizer : {tokenizer}")
     threshold = 0.2
     try:
         if model is None:
@@ -23,16 +28,23 @@ async def predict_service(journal: JournalSchema, model) -> JournalSchema:
 
         logger.info("Starting prediction process for journal")
 
+        # input_data = np.array([journal.journal]).astype("object")
+        encoded_sample = tokenize_data([journal.journal], tokenizer)
+
         # Predict emotions
-        input_data = np.array([journal.journal]).astype("object")
-        predictions = model.predict(input_data)
-        logger.debug(f"Predictions: {predictions}")
+        prediction = model.predict(
+            {
+                "input_ids": encoded_sample["input_ids"],
+                "attention_mask": encoded_sample["attention_mask"],
+            }
+        )
+        logger.debug(f"Predictions: {prediction}")
 
         # Filter predictions by confidence threshold
         emotion_classes = [emotion.value for emotion in Emotion]
         emotion_data = [
             EmotionItem(result=emotion_classes[i], confidence=round(float(conf), 3))
-            for i, conf in enumerate(predictions[0])
+            for i, conf in enumerate(prediction[0])
             if conf > threshold
         ]
         if not emotion_data:
@@ -48,3 +60,13 @@ async def predict_service(journal: JournalSchema, model) -> JournalSchema:
     except Exception as e:
         logger.error(f"Failed to predict: {e}", exc_info=True)
         return json_response(status_code=500, message="Something went wrong")
+
+
+def tokenize_data(texts, tokenizer, max_len=MAX_LENGTH):
+    return tokenizer(
+        list(texts),
+        max_length=max_len,
+        truncation=True,
+        padding="max_length",
+        return_tensors="tf",
+    )
