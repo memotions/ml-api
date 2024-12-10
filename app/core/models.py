97b1e54644c5bcd8d60aaf214737c1001e93bab6
GungIndi <gungindikusumaputra@gmail.com>
2024-12-10 12:33:20 +0800

    refactor: add download model func if there's no model in local

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,8 @@
 import gc
+import os
+import dotenv
 import tensorflow as tf
+from google.cloud import storage
 from app.core.logging_config import setup_logging
 from app.core.response import json_response
 
@@ -7,7 +10,23 @@ logger, _ = setup_logging()
 
 
 async def load_model(MODEL_URL):
+    BUCKET_NAME = os.getenv("BUCKET_NAME")
+    CLOUD_MODEL_PATH = os.getenv("CLOUD_MODEL_PATH")
     try:
+        if not os.path.exists(MODEL_URL):
+            if BUCKET_NAME and CLOUD_MODEL_PATH:
+                logger.debug(
+                    f"Model not found locally. Attempting to download from cloud storage..."
+                )
+                await download_model_from_cloud(
+                    BUCKET_NAME, CLOUD_MODEL_PATH, MODEL_URL
+                )
+            else:
+                logger.error(
+                    "Model not found locally and cloud storage details are not provided."
+                )
+                return json_response(500, "Something went wrong")
+
         logger.debug(f"Load model from {MODEL_URL}...")
         model = tf.keras.models.load_model(MODEL_URL)
         return model
@@ -20,3 +39,18 @@ async def load_model(MODEL_URL):
 async def unload_model(model):
     del model
     gc.collect()
+
+
+async def download_model_from_cloud(BUCKET_NAME, CLOUD_MODEL_PATH, destination):
+    try:
+        logger.debug(
+            f"Downloading model from cloud storage: {BUCKET_NAME}/{CLOUD_MODEL_PATH} to {destination}..."
+        )
+        client = storage.Client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(CLOUD_MODEL_PATH)
+        blob.download_to_filename(destination)
+        logger.debug("Model download completed.")
+    except Exception as e:
+        logger.error(f"Error downloading model from cloud: {e}", exc_info=True)
+        raise
